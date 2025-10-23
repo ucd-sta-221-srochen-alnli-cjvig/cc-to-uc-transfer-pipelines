c70e33d4f096c2ad6f0dcb0fb31e84dc6b2730aa
Cody Vig <cody.cjv@gmail.com>
2025-10-22 21:50:12 -0700

    Renamed {scripts/ -> api/}

--- src/ucd_sta_221_project/api/__init__.py
+++ src/ucd_sta_221_project/api/__init__.py
@@ -1,4 +1,4 @@
-from src.ucd_sta_221_project.scripts.college_scorecard import (
+from src.ucd_sta_221_project.api.college_scorecard import (
     get_scorecards_by_state,
     get_latest_student_scorecard_data_by_state,
     get_scorecard_by_college,
--- src/ucd_sta_221_project/api/college_scorecard.py
+++ src/ucd_sta_221_project/api/college_scorecard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import time
 
-from src.ucd_sta_221_project.scripts.utils import flatten_dict
+from src.ucd_sta_221_project.api.utils import flatten_dict
 
 def _get_api_key() -> str:
     """
